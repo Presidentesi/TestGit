54f4ad30c48f7147acd386618d324294d2c79832
William Cavalcante <cavalwc@gmail.com>
2016-05-24 00:45:01 -0300

    Commit classe Outra

--- src/es/esy/presidente/exemplos/testgit/Outra.java
+++ src/es/esy/presidente/exemplos/testgit/Outra.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testgit;
+package es.esy.presidente.exemplos.testgit;
 
 /**
  *
--- src/es/esy/presidente/exemplos/testgit/TestGit.java
+++ src/es/esy/presidente/exemplos/testgit/TestGit.java
@@ -1,12 +1,10 @@
 
-package testgit;
+package es.esy.presidente.exemplos.testgit;
 
 public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("Commit");
-        System.out.println("Commit2");
-        System.out.println("Commit3");
-    }
+}
     
 }
